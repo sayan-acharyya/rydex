72bb6da2020188494122e18a677b3ca24ab89600
sayan-acharyya <acharyyas735@gmail.com>
2026-04-20 19:47:32 +0530

    start working on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-     <div>
-     hi
+     <div className="w-full min-h-screen bg-white">
+      
      </div>
   );
 }
