6ef9c16d9b5621d1e260b0906b72ecc6da9dd2ed
sayan-acharyya <acharyyas735@gmail.com>
2026-04-27 01:46:19 +0530

    onboardui  done for become a partner

---
+++ src/app/partner/onboarding/bank/page.tsx
@@ -0,0 +1,139 @@
+
+'use client'
+import React from 'react'
+import { motion } from "motion/react";
+import { useRouter } from 'next/navigation';
+import { ArrowLeft, BadgeCheck, CheckCircle, CreditCard, Landmark, Phone, Smartphone } from 'lucide-react';
+
+
+const page = () => {
+    const router = useRouter();
+    return (
+        <div className='min-h-screen bg-white flex items-center justify-center px-4'>
+            <motion.div
+                initial={{ opacity: 0, y: 28 }}
+                animate={{ opacity: 1, y: 0 }}
+                className='w-full max-w-xl bg-white rounded-3xl border border-gray-200
+            shadow-[0_25px_70px_rgba(0,0,0,0.15)] p-6 sm:p-8'
+            >
+                <div className='relative text-center'>
+                    <button onClick={() => router.back()} className='absolute left-0 top-0 w-9 h-9 rounded-full border border-gray-300 
+                    flex items-center justify-center hover:bg-gray-100 transition'>
+                        <ArrowLeft size={18} />
+                    </button>
+
+                    <p className='text-xs text-gray-500 font-medium'>
+                        step 3 of 3
+                    </p>
+
+                    <h1 className='text-2xl font-bold mt-1'>
+                        Bank & Payout Setup
+                    </h1>
+
+                    <p className='text-sm text-gray-500 mt-2'>
+                        Used for partner payouts
+                    </p>
+
+                </div>
+
+                <div className='mt-8 space-y-6'>
+                    <div>
+                        <label htmlFor="ahn" className='text-xs font-semibold text-gray-500'>
+                            Account holder name
+                        </label>
+                        <div className='flex items-center gap-2 mt-2'>
+                            <div className='text-gray-400'><BadgeCheck /></div>
+                            <input
+                                id='ahn'
+                                type='text'
+                                placeholder='As per bank records'
+                                className='flex-1 border-b pb-2 text-sm focus:outline-none border-gray-300 focus:border-black'
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="an" className='text-xs font-semibold text-gray-500'>
+                            Bank account number
+                        </label>
+                        <div className='flex items-center gap-2 mt-2'>
+                            <div className='text-gray-400'><CreditCard /></div>
+                            <input
+                                id='an'
+                                type='text'
+                                placeholder='Enter account number'
+                                className='flex-1 border-b pb-2 text-sm focus:outline-none border-gray-300 focus:border-black'
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="ahn" className='text-xs font-semibold text-gray-500'>
+                            IFSC code
+                        </label>
+                        <div className='flex items-center gap-2 mt-2'>
+                            <div className='text-gray-400'><Landmark /></div>
+                            <input
+                                id='ahn'
+                                type='text'
+                                placeholder='HDFC0001234'
+                                className='flex-1 border-b pb-2 text-sm focus:outline-none border-gray-300 focus:border-black'
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="ahn" className='text-xs font-semibold text-gray-500'>
+                            Mobile number
+                        </label>
+                        <div className='flex items-center gap-2 mt-2'>
+                            <div className='text-gray-400'><Phone /></div>
+                            <input
+                                id='ahn'
+                                type='text'
+                                placeholder='10 digit mobile number'
+                                className='flex-1 border-b pb-2 text-sm focus:outline-none border-gray-300 focus:border-black'
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="ahn" className='text-xs font-semibold text-gray-500'>
+                            UPI ID (optional)
+                        </label>
+                        <div className='flex items-center gap-2 mt-2'>
+
+                            <input
+                                id='ahn'
+                                type='text'
+                                placeholder='name@upi'
+                                className='flex-1 border-b pb-2 text-sm focus:outline-none border-gray-300 focus:border-black'
+                            />
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className='mt-8 flex items-start gap-3 text-xs text-gray-500'>
+                    <CheckCircle size={16} className='mt-0.5' />
+                    <p>
+                        Bank details are verified before first payout.
+                        This usually takes 24-48 hours
+                    </p>
+                </div>
+
+                <motion.div
+                    whileHover={{ scale: 1.02 }}
+                    whileTap={{ scale: 0.97 }}
+                     className='mt-8 w-full h-14 rounded-2xl bg-black text-white font-semibold flex
+                    items-center justify-center gap-2 disabled:opacity-40 transition'
+                >
+                    Continue
+                </motion.div>
+
+            </motion.div>
+        </div >
+    )
+}
+
+export default page
--- src/app/partner/onboarding/vehicle/page.tsx
+++ src/app/partner/onboarding/vehicle/page.tsx
@@ -1,9 +1,157 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
+import { motion } from "motion/react";
+import { ArrowLeft, Bike, Car, Package, Truck } from 'lucide-react';
+import { useRouter } from 'next/navigation';
 
 const page = () => {
+    const router = useRouter();
+
+    const VEHICLES = [
+        {
+            id: "bike",
+            label: "Bike",
+            icon: Bike,
+            description: "2 wheeler"
+        },
+        {
+            id: "auto",
+            label: "Auto",
+            icon: Car,
+            description: "3 wheeler"
+        },
+        {
+            id: "car",
+            label: "Car",
+            icon: Car,
+            description: "4 wheeler"
+        },
+        {
+            id: "loading",
+            label: "Loading",
+            icon: Package,
+            description: "Small goods"
+        },
+        {
+            id: "truck",
+            label: "Truck",
+            icon: Truck,
+            description: "Heavy transport"
+        }
+    ];
+
+    const [vehicleType, setVehicleType] = useState("");
+    const [vehicleNumber, setVehicleNumber] = useState("");
+    const [vehicleModel, setVehicleModel] = useState("");
+
+
     return (
-        <div>
-            vehicle on boarding
+        <div className='min-h-screen bg-white flex items-center justify-center px-4'>
+            <motion.div
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.4 }}
+                className='w-full max-w-xl bg-white rounded-3xl border border-gray-200 
+                shadow-[0_25px_70px_rgba(0,0,0,0.15)] p-6 sm:p-8'
+            >
+                <div className='relative text-center'>
+                    <button onClick={() => router.back()} className='absolute left-0 top-0 w-9 h-9 rounded-full border border-gray-300 
+                    flex items-center justify-center hover:bg-gray-100 transition'>
+                        <ArrowLeft size={18} />
+                    </button>
+
+                    <p className='text-xs text-gray-500 font-medium'>
+                        step 1 of 3
+                    </p>
+
+                    <h1 className='text-2xl font-bold mt-1'>
+                        Vehicle Details
+                    </h1>
+
+                    <p className='text-sm text-gray-500 mt-2'>
+                        Add your vehicle information
+                    </p>
+
+                </div>
+
+                <div className='mt-8 space-y-6'>
+                    <div>
+                        <p className='text-xs font-semibold text-gray-500 mb-3'>Vehicle Type</p>
+                        <div className='grid grid-cols-2 sm:grid-cols-3 gap-3'>
+                            {VEHICLES.map((v, i) => {
+                                const Icon = v.icon
+                                const active = vehicleType == v.id
+                                return (
+                                    <motion.div
+                                        key={v.id}
+                                        whileHover={{ scale: 1.05 }}
+                                        whileTap={{ scale: 0.96 }}
+                                        onClick={() => setVehicleType(v.id)}
+                                        className={`rounded-2xl border p-4 flex flex-col items-center gap-2 transition
+                                            ${active ?
+                                                "bg-black text-white border-black"
+                                                : "border-gray-200 hover:border-black"
+                                            }`}
+                                    >
+                                        <div className={`w-11 h-11 rounded-full flex items-center justify-center 
+                                            ${active ? "bg-white text-black" : "bg-black text-white"}`}>
+                                            <Icon />
+                                        </div>
+
+                                        <div className='text-sm font-semibold'>
+                                            {v.label}
+                                        </div>
+                                        <p className={`text-xs ${active ? "text-gray-300" : "text-gray-500"}`}>
+                                            {v.description}
+                                        </p>
+                                    </motion.div>
+                                )
+                            })}
+                        </div>
+                    </div>
+
+                    <div>
+                        <label
+                            className="  text-xs font-semibold text-gray-700 mb-1"
+                            htmlFor="vn">Vehicle Number</label>
+                        <input
+                            type="text"
+                            value={vehicleNumber}
+                            onChange={(e) => setVehicleNumber(e.target.value)}
+                            placeholder='MH12AB1234'
+                            id='vn'
+                            className='mt-2 w-full border-b border-gray-300  
+                        pb-2 text-sm focus:outline-none focus:border-black transition'
+                        />
+                    </div>
+
+                    <div>
+                        <label
+                            className="  text-xs font-semibold text-gray-700 mb-1"
+                            htmlFor="vm">Vehicle Model</label>
+                        <input
+                            type="text"
+                            value={vehicleModel}
+                            onChange={(e) => setVehicleModel(e.target.value)}
+                            placeholder='Tata Ace'
+                            id='vm'
+                            className='mt-2 w-full border-b border-gray-300  
+                        pb-2 text-sm focus:outline-none focus:border-black transition'
+                        />
+                    </div>
+                </div>
+
+                <motion.button
+                    whileHover={{ scale: 1.02 }}
+                    whileTap={{ scale: 0.97 }}
+                    className='mt-8 w-full h-14 rounded-2xl bg-black text-white font-semibold flex
+                    items-center justify-center gap-2 disabled:opacity-40 transition'
+                >
+                    Continue
+                </motion.button>
+
+            </motion.div>
         </div>
     )
 }
