6c64ee35c848b2b6db1dd58ad80ebebda3826527
sayan-acharyya <acharyyas735@gmail.com>
2026-05-03 13:44:40 +0530

    api done

--- src/app/api/admin/reviews/partner/[id]/route.ts
+++ src/app/api/admin/reviews/partner/[id]/route.ts
@@ -1,3 +1,9 @@
+import { auth } from "@/auth";
+import connectDb from "@/lib/db";
+import PartnerBank from "@/models/partnerBank.model";
+import PartnerDocs from "@/models/partnerDocs.model";
+import User from "@/models/user.model";
+import Vehicle from "@/models/vehicle.model";
 import { NextRequest } from "next/server";
 
 
@@ -5,5 +11,42 @@ export async function GET(
     req: NextRequest,
     context: { params: Promise<{ id: string }> }
 ) {
+    try {
+        await connectDb();
 
+        // ✅ Auth check
+        const session = await auth();
+        if (!session || !session.user?.email || session.user.role !== "admin") {
+            return Response.json({ message: "Unauthorized" }, { status: 401 });
+        }
+
+        const partnerId = (await context.params).id;
+
+        const partner = await User.findById(partnerId);
+
+
+        if (!partner || partner.role !== "partner") {
+            return Response.json({ message: "partner  not found" }, { status: 404 });
+        }
+
+        const vehicle = await Vehicle.findOne({ owner: partnerId });
+        const documents = await PartnerDocs.findOne({ owner: partnerId });
+        const bank = await PartnerBank.findOne({ owner: partnerId });
+
+        return Response.json(
+            {
+                vehicle: vehicle || null,
+                documents: documents || null,
+                bank: bank || null
+
+            },
+            { status: 200 }
+        )
+    } catch (error) {
+        return Response.json(
+            { message: `partner get error ${error}` }
+            , { status: 500 });
+    }
 }
+
+//2:57:55
